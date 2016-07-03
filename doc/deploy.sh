553c32d5ae268ea6f819bc0800dd3bf63106adef
Touched <jtouched@gmail.com>
2016-07-03 15:12:53 +0200

    Add documentation generation

---
+++ doc/deploy.sh
@@ -0,0 +1,25 @@
+set -e
+
+REPO_PATH=git@github.com:Touched/PokeAGB.git
+HTML_PATH=doc/html
+COMMIT_USER="Travis CI"
+COMMIT_EMAIL="travis@travis-ci.org"
+
+rm -rf $HTML_PATH
+mkdir -p $HTML_PATH
+git clone -b gh-pages "${REPO_PATH}" --single-branch $HTML_PATH
+
+cd $HTML_PATH
+git rm -rf .
+touch .nojekyll
+cd ..
+
+make
+
+cd $HTML_PATH
+git add .
+git config user.name $COMMIT_USER
+git config user.email $COMMIT_EMAIL
+git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" \
+    -m "Commit: ${TRAVIS_COMMIT}"
+git push origin gh-pages

406fb56889e4888d25bc29f84a7bd2af43a65b0e
Touched <jtouched@gmail.com>
2016-07-08 21:03:30 +0200

    Fix macros header

--- include/pokeagb/macros.h
+++ include/pokeagb/macros.h
@@ -1,9 +1,26 @@
-#ifndef POKEAGB_MACRO_H_
-#define POKEAGB_MACRO_H_
+/*
+ * This file is part of PokeAGB.
+ *
+ * PokeAGB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PokeAGB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PokeAGB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef POKEAGB_MACROS_H_
+#define POKEAGB_MACROS_H_
 
 #define LONG_CALL __attribute__((long_call))
 #define ASSERT_SIZEOF(type, size) _Static_assert(sizeof(type) == size, \
                                                  "Expected size of '" \
                                                  #type "' to be " #size ".")
 
-#endif /* POKEAGB_MACRO_H_ */
+#endif /* POKEAGB_MACROS_H_ */

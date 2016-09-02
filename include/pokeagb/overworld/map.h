4bdce5bae0618b03c51d24ce83eb782d843a8b77
Touched <jtouched@gmail.com>
2016-09-02 20:51:38 +0200

    Add overworld map header

---
+++ include/pokeagb/overworld/map.h
@@ -0,0 +1,413 @@
+/**
+ * @file
+ * @brief Overworld maps.
+ */
+
+#ifndef POKEAGB_OVERWORLD_MAP_H_
+#define POKEAGB_OVERWORLD_MAP_H_
+
+#include <pokeagb/types.h>
+#include <pokeagb/common.h>
+#include <pokeagb/version.h>
+#include <pokeagb/overworld/item.h>
+#include <pokeagb/overworld/npc.h>
+#include <pokeagb/graphics/sprites.h>
+
+POKEAGB_BEGIN_DECL
+
+// TODO: Move this structure elsewhere.
+
+/**
+ * Tile map tile.
+ */
+struct Tile {
+    /**
+     * Character number.
+     */
+    u16 character : 10;
+
+    /**
+     * Horizontal Flip.
+     */
+    u16 h_flip : 1;
+
+    /**
+     * Vertical Flip.
+     */
+    u16 v_flip : 1;
+
+    /**
+     * Palette number.
+     */
+    u16 palette : 4;
+};
+
+/**
+ * A tile on the map.
+ */
+struct MapTile {
+    u16 tile : 10;
+    u16 permission : 6;
+};
+
+ASSERT_SIZEOF(struct MapTile, 2);
+
+#ifdef VERSION_FRLG
+#define MAP_BLOCKSET_PRIMARY_MAX_BLOCKS 640
+#define MAP_BLOCKSET_SECONDARY_MAX_BLOCKS 384
+#define MAP_BLOCKSET_MAX_BLOCKS (MAP_BLOCKSET_PRIMARY_MAX_BLOCKS + MAP_BLOCKSET_SECONDARY_MAX_BLOCKS)
+#endif
+
+/**
+ * The behavior of the block.
+ */
+struct MapBlockBehavior {
+    /**
+     * Block role.
+     */
+    u32 role : 9;
+
+    /**
+     * Unknown. 2 for grass, 4 for water.
+     */
+    u32 field1 : 5;
+    u32 field2 : 4;
+    u32 field3 : 6;
+
+    /**
+     * The encounter type of the block.
+     */
+    enum {
+        MAP_BLOCK_ENCOUNTER_NONE,
+        MAP_BLOCK_ENCOUNTER_GRASS,
+        MAP_BLOCK_ENCOUNTER_WATER,
+    } encounter : 3;             /* Encounter related. */
+
+    u32 field5 : 2;
+
+    /**
+     * What background layers block tiles are drawn on.
+     */
+    enum {
+        /**
+         * Normal mode. Layer 2 goes above NPCs.
+         *
+         * Block layer 1 -> BG2
+         * Block layer 2 -> BG1
+         */
+        MAP_BLOCK_BG_21,
+
+        /**
+         * NPCs appear on top of both layers.
+         *
+         * Block layer 1 -> BG3
+         * Block layer 2 -> BG2
+         */
+        MAP_BLOCK_BG_32,
+
+        /**
+         * Seemingly unused.
+         *
+         * Block layer 1 -> BG3
+         * Block layer 2 -> BG1
+         */
+        MAP_BLOCK_BG_31,
+    } backgrounds: 2;
+
+    u32 field7 : 1;
+};
+
+ASSERT_SIZEOF(struct MapBlockBehavior, 4);
+
+/**
+ * A single map block.
+ */
+struct MapBlock {
+    struct Tile layer[2][4];
+};
+
+ASSERT_SIZEOF(struct MapBlock, 0x10);
+
+/**
+ * Blocks from which the map is constructed.
+ */
+struct MapBlockset {
+    /**
+     * Whether the tiles are compressed or not.
+     */
+    bool compressed;
+
+    /**
+     * Whether this tileset is to be used as a secondary tileset or primary tileset.
+     */
+    bool secondary;
+    u16 padding;
+
+    /**
+     * Tiles used to build blocks.
+     */
+    void* tiles;
+
+    /**
+     * Palettes for the blockset.
+     */
+    void* palettes;
+
+    /**
+     * Block description.
+     */
+    struct MapBlock* blocks;
+
+    /**
+     * Tileset initialization function. Called to set up animation functions.
+     */
+    void (*function)(void);
+
+    /**
+     * Block behaviours.
+     */
+    struct MapBlockBehavior* behaviors;
+};
+
+ASSERT_SIZEOF(struct MapBlockset, 0x18);
+
+/**
+ * Map data.
+ */
+struct MapData {
+    u32 width;
+    u32 height;
+    struct MapTile* border;
+    struct MapTile* data;
+    struct MapBlockset* blocksets[2];
+#ifdef VERSION_FRLG
+    u8 border_width;
+    u8 border_height;
+#endif
+};
+
+#ifdef VERSION_FRLG
+ASSERT_SIZEOF(struct MapData, 0x1C);
+#else
+ASSERT_SIZEOF(struct MapData, 0x18);
+#endif
+
+/**
+ * Warps.
+ */
+struct WarpData {
+    /**
+     * Target map bank.
+     */
+    u8 bank;
+
+    /**
+     * Target map.
+     */
+    u8 map;
+
+    /**
+     * Target warp id.
+     */
+    u8 id;
+
+    /**
+     * Warp co-ordinates.
+     */
+    struct Coords16 coordinates;
+};
+
+ASSERT_SIZEOF(struct WarpData, 8);
+
+/**
+ * Scripts that are run when the event is stepped on.
+ */
+struct TriggerData {
+    /**
+     * Trigger co-ordinates.
+     */
+    struct Coords16 coordinates;
+
+    /**
+     * Talking level.
+     */
+    u16 height;
+
+    /**
+     * Trigger variable.
+     */
+    u16 variable;
+
+    /**
+     * Trigger variable value. Runs the trigger if variable is equal to this value.
+     */
+    u16 value;
+    u16 padding;
+
+    /**
+     * Script to run.
+     */
+    u8* script;
+};
+
+ASSERT_SIZEOF(struct TriggerData, 0x10);
+
+enum SignpostType {
+    /**
+     * Regular signpost script.
+     */
+    MAP_SIGNPOST_SCRIPT,
+
+    /**
+     * Run script if the player is facing up.
+     */
+    MAP_SIGNPOST_SCRIPT_UP,
+
+    /**
+     * Run script if the player is facing down.
+     */
+    MAP_SIGNPOST_SCRIPT_DOWN,
+
+    /**
+     * Run script if the player is facing left.
+     */
+    MAP_SIGNPOST_SCRIPT_LEFT,
+
+    /**
+     * Run script if the player is facing right.
+     */
+    MAP_SIGNPOST_SCRIPT_RIGHT,
+
+    /**
+     * Hidden item. Behaves identically to 5 and 6.
+     */
+    MAP_SIGNPOST_HIDDEN_ITEM = 7,
+
+#ifdef VERSION_RSE
+    /**
+     * A secret base entrace. (CHECK)
+     */
+    MAP_SIGNPOST_SECRET_BASE,
+#endif
+};
+
+struct SignpostData {
+    /**
+     * Signpost co-ordinates.
+     */
+    struct Coords16 coordinates;
+
+    /**
+     * Talking level.
+     */
+    u16 height;
+
+    /**
+     * The type of signpost.
+     */
+    enum SignpostType type;
+
+    union {
+        /**
+         * Script to run. Only for script type signposts.
+         */
+        u8* script;
+
+        /**
+         * Hidden item configuration. Only for hidden item type signposts.
+         */
+        struct {
+            /**
+             * Item index.
+             */
+            enum Item index;
+
+            /**
+             * Hidden item ID. Should be globally unique.
+             */
+            u8 flag;
+
+            /**
+             * The quantity to give.
+             */
+            u8 amount : 7;
+
+            /**
+             * If true, no script will be run.
+             */
+            bool skip : 1;
+        } item;
+    };
+};
+
+ASSERT_SIZEOF(struct SignpostData, 0xC);
+
+/**
+ * Events on the map.
+ */
+struct MapEvents {
+    u8 num_npcs;
+    u8 num_warps;
+    u8 num_triggers;
+    u8 num_signposts;
+    struct RomNpc* npcs;
+    struct WarpData* warps;
+    struct TriggerData* triggers;
+    struct SignpostData* signposts;
+};
+
+ASSERT_SIZEOF(struct MapEvents, 0x14);
+
+enum MapConnectionType {
+    MAP_CONNECTION_NOTHING,
+    MAP_CONNECTION_DOWN,
+    MAP_CONNECTION_UP,
+    MAP_CONNECTION_LEFT,
+    MAP_CONNECTION_RIGHT,
+    MAP_CONNECTION_DIVE,
+    MAP_CONNECTION_EMERGE,
+};
+
+/**
+ * How the map is connected to other maps.
+ */
+struct MapConnection {
+    enum MapConnectionType direction;
+    s32 offset;
+    u8 bank;
+    u8 map;
+    u16 padding;
+};
+
+ASSERT_SIZEOF(struct MapConnection, 0xC);
+
+struct MapConnections {
+    u32 number;
+    struct MapConnection* data;
+};
+
+/**
+ * A map.
+ */
+struct MapHeader {
+    struct MapData* data;
+    struct MapEvents* events;
+    void* scripts;
+    struct MapConnections* connections;
+    u16 music;
+    u16 map_index;
+    u8 name;
+    u8 cave;
+    u8 weather;
+    u8 light;
+    u8 field_18;
+    u8 escape_rope;
+    u8 show_name;
+    u8 battle_type;
+};
+
+ASSERT_SIZEOF(struct MapHeader, 0x1C);
+
+POKEAGB_END_DECL
+
+#endif /* POKEAGB_OVERWORLD_MAP_H_ */

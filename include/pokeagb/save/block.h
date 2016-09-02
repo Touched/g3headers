48239bad7c23bc212b323515594d65f91efc392a
Touched <jtouched@gmail.com>
2016-09-02 22:19:07 +0200

    Add save block structures

---
+++ include/pokeagb/save/block.h
@@ -0,0 +1,159 @@
+/**
+ * @file
+ * @brief Game save data.
+ */
+
+#ifndef POKEAGB_SAVE_BLOCK_H_
+#define POKEAGB_SAVE_BLOCK_H_
+
+#include <pokeagb/types.h>
+#include <pokeagb/common.h>
+#include <pokeagb/version.h>
+#include <pokeagb/core/string.h>
+#include <pokeagb/overworld/npc.h>
+#include <pokeagb/overworld/map.h>
+#include <pokeagb/pokemon/data.h>
+
+POKEAGB_BEGIN_DECL
+
+// TODO: Emerald and Ruby/Sapphire saves
+
+/**
+ * An entry in the log for the "previously on your quest" feature.
+ */
+struct QuestLogStory {
+  u8 active;
+  u8 field_1[1638];
+  u8 field_667;
+};
+
+ASSERT_SIZEOF(struct QuestLogStory, 0x668);
+
+/**
+ * Main save game data.
+ */
+#pragma pack(push, 1)
+struct SaveBlock1 {
+    struct Coords16 camera_position;
+    struct WarpData location;
+    struct WarpData saved_warp;
+    struct WarpData field_14;
+    struct WarpData whiteout_related;
+    struct WarpData save_warp_2;
+    u16 field_2C;
+    u8 weather_probably;
+    u8 gap_2F[1];
+    u8 flash_used_on_map;
+    u8 gap_31[1];
+    u16 mapdata_idx_plus_1;
+    u8 num_party_pokemon;
+    u8 gap_35[603];
+    u32 money_maybe;
+    u8 pcitems[124];
+    u32 bag_probably[42];
+    u8 ghu_2_x3B8[120];
+    u32 field_430[13];
+    u8 ghu_1_x464[232];
+    u8 field_54C[172];
+    u8 pokemon_flags_1[52];
+    u8 field_62C[12];
+    u16 field_638;
+    u32 field_63A;
+    u8 field_63E[98];
+    struct NpcState npc_states[16];
+    struct RomNpc rom_npcs[64];
+    u8 field_EE0[64];
+    u32 field_F20;
+    u8 field_F24[220];
+    u16 var_4000[256];
+    u8 field_1200[256];
+    struct QuestLogStory questlog[4];
+    u8 field_2CA0[48];
+    struct NpcState field_2CD0[16];
+    u8 field_2F10[112];
+    u32 daycare_data;
+    u8 field_2F84[332];
+    u8 field_30D0[28];
+    u8 field_30EC[28];
+    u16 engima_berry_data[9];
+    u8 field_311A[458];
+    u8 mevent_related_1[332];
+    u32 field_3430;
+    u8 mevent_related_2[332];
+    u8 field_3580[156];
+    u32 ereader_script;
+    u8 field_3620[4];
+    u8 field_3624[1012];
+    u8 pokemon_flags_2[52];
+    u8 rival_name[8];
+    u8 field_3A54[736];
+    u8 field_3D34[14];
+    u8 field_3D42[12];
+    u8 field_3D4E[153];
+    u8 last_field;
+};
+#pragma pack(pop)
+
+ASSERT_SIZEOF(struct SaveBlock1, 0x3DE8);
+
+/**
+ * Trainer and miscellaneous save data.
+ */
+struct SaveBlock2 {
+    u8 name[8];
+    u8 gender;
+    u8 field_9;
+    u16 trainerid;
+    u16 secretid;
+    u16 playtime_hours;
+    u8 playtime_minutes;
+    u8 playtime_seconds;
+    u8 playtime_frames;
+    u8 options_button_style;
+    u8 options_text_speed_maybe;
+    u8 field_15[7];
+    u32 field_1C;
+    u8 field_20[8];
+    u8 pokemon_flags_3[52];
+    u8 pokemon_flags_4[52];
+    u8 field_90[1044];
+    u8 fourCharacters[4];
+    u8 field_4A8[1008];
+    u8 mapdata[1672];
+    u32 bag_item_quantity_xor_value;
+    u8 field_F24[127];
+    u8 last_byte_in_sav2;
+};
+
+ASSERT_SIZEOF(struct SaveBlock2, 0xFA4);
+
+/**
+ * Pokemon storage related save data.
+ */
+struct SaveBlock3 {
+  u8 preferred_box;
+  u8 gap_1[3];
+  struct PokemonBase lanettes_boxes[420];
+  pchar box_names[14][9];
+};
+
+ASSERT_SIZEOF(struct SaveBlock3, 0x83C2);
+
+/**
+ * @address{BPRE,03005008}
+ */
+extern struct SaveBlock1* saveblock1;
+
+/**
+ * @address{BPRE,0300500C}
+ */
+extern struct SaveBlock2* saveblock2;
+
+/**
+ * @address{BPRE,03005010}
+ */
+extern struct SaveBlock3* saveblock3;
+
+POKEAGB_END_DECL
+
+#endif /* POKEAGB_SAVE_BLOCKH_ */

9142fd2eda987defc37e7d49258a63a4e309a0f3
Touched <jtouched@gmail.com>
2016-10-12 10:04:41 +0200

    Cross-platform static assert

--- include/pokeagb/common.h
+++ include/pokeagb/common.h
@@ -3,23 +3,43 @@
 
 #include <stddef.h>
 
+#define VALUE_TO_STRING(x) #x
+#define VALUE(x) VALUE_TO_STRING(x)
+#define VAR_NAME_VALUE(var) #var "=" VALUE(var)
+
+
 #define POKEAGB_EXTERN extern __attribute__((long_call))
+
+#ifdef __cplusplus
+#define STATIC_ASSERT(expr, message) static_assert(expr, message);
+#elif defined(__STDC_VERSION__)
+#if __STDC_VERSION__ == 201112L
+#define STATIC_ASSERT(expr, message) _Static_assert(expr, message);
+#else
+#endif
+#endif
+
+#ifndef STATIC_ASSERT
+#error "No static assert available for this platform"
+#endif
 
 /* TODO: Add more asserts to the code. */
 
 /**
  * Assert (at compile time) that a type is of a given size.
  */
-#define ASSERT_SIZEOF(type, size) _Static_assert(sizeof(type) == size, \
-                                                 "Expected size of '" \
-                                                 #type "' to be " #size ".")
+#define ASSERT_SIZEOF(type, size)                                     \
+    _Static_assert(sizeof(type) == size,                              \
+                   "Expected size of `"                               \
+                   #type "` to be " #size ".")
 
 /**
  * Assert (at compile time) that a member occurs at a given offset.
  */
-#define ASSERT_OFFSETOF(type, member, offset) _Static_assert(offsetof(type, member) == offset, \
-                                                             "Expected `" #member "` in `" \
-                                                             #type "` to be at offset " #offset ".")
+#define ASSERT_OFFSETOF(type, member, offset)                           \
+    STATIC_ASSERT(offsetof(type, member) == offset,                     \
+                  "Expected `" #member "` in `"                         \
+                  #type "` to be at offset " #offset ".")
 
 #ifdef __cplusplus
 #define POKEAGB_BEGIN_DECL extern "C" {
--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
 CC="arm-none-eabi-gcc"
-CFLAGS="-Wall -Werror -fno-builtin -I include"
+CFLAGS="-Wall -Werror -Wfatal-errors -fno-builtin -I include -std=c11"
 
 $CC $CFLAGS `find include -name '*.h'` -o /dev/null
